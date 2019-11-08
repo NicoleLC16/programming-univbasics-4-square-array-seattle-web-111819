199a69c6e7e1009e5a32c2433665adb164fce76d
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-08 08:21:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array (array)
   counter = 0
  new_array = []
   while counter < array.length do
-      puts array[counter]
+      puts (array[counter]*array[counter])
       counter+=1
   end
      p new_array.push(array)
